291177777fa843323995c24fe517f6a1b72bcf13
jovan.petrovic <jovanloki@gmail.com>
2026-05-06 11:07:56 +0200

    Reset source files in order to prepare for reactive forms implementation.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,54 +1,9 @@
-import {
-  afterNextRender,
-  Component,
-  DestroyRef,
-  inject,
-  viewChild,
-} from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  // Using viewChild to get a reference to the form in html
-  private form = viewChild.required<NgForm>('form');
-
-  private destroyRef = inject(DestroyRef);
-
-  constructor() {
-    afterNextRender(() => {
-      const subscription = this.form()
-        .valueChanges?.pipe(debounceTime(500))
-        .subscribe({
-          next: (value) =>
-            window.localStorage.setItem(
-              'saved-login-form',
-              JSON.stringify({ email: value.email }),
-            ),
-        });
-      this.destroyRef.onDestroy(() => {
-        subscription?.unsubscribe();
-      });
-    });
-  }
-
-  onSubmit(form: NgForm) {
-    if (form.invalid) {
-      return;
-    }
-
-    const email = form.value.email;
-    const password = form.value.password;
-
-    console.log(form);
-
-    console.log('Email:', email);
-    console.log('Password:', password);
-  }
-}
+export class LoginComponent {}
