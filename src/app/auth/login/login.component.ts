623023ad58cd4e977872adb2e68c6ec5f91c2d74
jovan.petrovic <jovanloki@gmail.com>
2026-05-06 11:36:41 +0200

    Made a simplest reactive form with two fields. Connected form using ts and html.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  // Define a reactive form with email and password fields
+  form = new FormGroup({
+    // Initial values are defined in the parentheses of the FormControl constructor
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {}
+}
