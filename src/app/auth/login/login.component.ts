371a0acbbad72dda493ab98f214a376cdbed73b8
jovan.petrovic <jovanloki@gmail.com>
2026-05-11 09:43:36 +0200

    Implemented simple validators in reactive form.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +17,37 @@ export class LoginComponent {
   // Define a reactive form with email and password fields
   form = new FormGroup({
     // Initial values are defined in the parentheses of the FormControl constructor
-    email: new FormControl(''),
-    password: new FormControl(''),
+    // Validators can also be added in the same place, for example: new FormControl('', Validators.required)
+    //
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.minLength(6), Validators.required],
+    }),
   });
 
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  // validator can also be added later in form submission ie. this.form.get('email').setValidators(Validators.required);
   onSubmit() {
     console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
   }
 }
